3eaa2e30125c2f56a71cc251b1d3c0c6d76f70d4
mlizchap <mlizchap@gmail.com>
2018-09-11 06:05:54 -0400

    .

--- app.js
+++ app.js
@@ -18,7 +18,19 @@ const app = express();
 //     .once('open',() => { console.log('db open'); })
 //     .on('error', () => (error) => console.warn('Warning', error))
 
-const db = mongoose.connect('mongodb://user:Kramer22!@ds151612.mlab.com:51612/recipedb');
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  
+    db = database;
+    
+    var server = app.listen(process.env.PORT || 8080, function () {
+      var port = server.address().port;
+      console.log("App now running on port", port);
+    });
+    
 
 // routes 
 router(app);
